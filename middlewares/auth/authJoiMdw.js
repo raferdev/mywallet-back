8572539bc2d7037bc85bea7ea2c8b587253c3b51
Rafael <rafinha_fern@hotmail.com>
2022-05-09 17:59:43 -0300

    tirando a restrição do input tão específicas pra facilitar os testes

--- middlewares/auth/authJoiMdw.js
+++ middlewares/auth/authJoiMdw.js
@@ -2,11 +2,11 @@ import Joi from "joi";
 export default async function authJoiMdw(req, res, next) {
   const authReq = req.body;
   const authScheme = Joi.object({
-    name: Joi.string().min(3).max(10).required(),
+    name: Joi.string().min(3).max(20).required(),
     email: Joi.string()
       .email({ tlds: { allow: false } })
       .required(),
-    password: Joi.string().min(8).max(16).required(),
+    password: Joi.string().min(3).max(20).required(),
   });
   const authValidation = authScheme.validate(authReq, { abortEarly: false });
   if (authValidation.error) {
