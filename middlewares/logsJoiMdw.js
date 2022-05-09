b417e6da1918b211f5481604b67f4fb7e05b6c41
Rafael <rafinha_fern@hotmail.com>
2022-05-09 18:09:44 -0300

    fazendo a mesma retirada de restrição de minimo e maximo do input do deposito e pagamento, já que estou aqui, também pela correção, já que não tem descrição do formato pro usuario

--- middlewares/logsJoiMdw.js
+++ middlewares/logsJoiMdw.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 export default async function logsJoiMdw(req, res, next) {
   const logReq = req.body;
   const logScheme = Joi.object({
-    type: Joi.string().min(10).max(100).required(),
+    type: Joi.string().min(1).max(100).required(),
     value: Joi.number().required(),
   });
   const logValidation = logScheme.validate(logReq, { abortEarly: false });
